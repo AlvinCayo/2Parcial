3653752afc6a2d27cdc93a1e2f0499fe540c0d8b
Alvin Cayo <cayoalvin991@gmail.com>
2023-10-20 12:53:53 -0400

    agregando los iconos

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,22 @@ import { Profile } from "./components/profile";
 import { User } from "./components/user";
 import { Id } from "./components/id";
 import { Banner } from "./components/banner";
+import { Container_icon } from "./components/container_icon";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="card1">
-      <Banner></Banner>
+        <Banner></Banner>
         <div className="card2">
-          
           <Profile></Profile>
           <div className="ord">
             <User></User>
             <Id></Id>
+          </div>
+          <div className="container">
+            <Container_icon></Container_icon>
           </div>
         </div>
       </div>
